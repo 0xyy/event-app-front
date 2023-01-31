bd3af3945a2acdf238760d9bef0c8c8e42f19c18
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-01-31 10:16:50 +0100

    Added basic main navigation.

---
+++ src/components/layout/Navigation.tsx
@@ -0,0 +1,29 @@
+import styles from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
+
+export const Navigation = () => {
+    return <nav className={styles.menu}>
+        <ul>
+            <li>
+                <NavLink
+                    to='/'
+                    className={({ isActive }) =>
+                        isActive ? styles.active : undefined
+                    }
+                >
+                    Wydarzenia
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to='/add-eventū'
+                    className={({ isActive }) =>
+                        isActive ? styles.active : undefined
+                    }
+                >
+                    Dodaj wydarzenie
+                </NavLink>
+            </li>
+        </ul>
+    </nav>;
+};
