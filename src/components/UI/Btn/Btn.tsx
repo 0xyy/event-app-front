6b3aa1e7c8155b4d5e7c431a53e57ef0814b148a
Tommy Phan <tommyphan2004.tp@gmail.com>
2023-02-01 11:24:06 +0100

    Moved btn component to the directory and added submit prop.

---
+++ src/components/UI/Btn/Btn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Btn.module.css';
+
+interface Props {
+    children: React.ReactNode;
+    submit?: boolean;
+}
+
+export const Btn = ({ children, submit }: Props) => {
+    return <button className={styles.btn} type={submit ? 'submit' : undefined}>{children}</button>;
+}
